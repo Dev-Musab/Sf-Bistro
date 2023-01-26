a32a439bdf4c691c8a531622b1e6e01a6da805b2
Dev-musab <Dev.tatatech@gmail.com>
2023-01-26 17:34:13 +0400

    second commit

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/pages/home/home.component';
@@ -27,7 +28,9 @@ import { NotFoundComponent } from './components/partials/not-found/not-found.com
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RatingModule
+    RatingModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]

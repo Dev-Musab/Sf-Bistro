e77f90fdb9ee2a4cde5533e273f9b922c73d571d
Dev-musab <Dev.tatatech@gmail.com>
2023-01-27 15:33:42 +0400

    thrid commit 12.1 login

--- backend/src/data.ts
+++ backend/src/data.ts
@@ -214,3 +214,20 @@ export const sample_tags:any[] = [
   { name: 'CHINESE', count: 3 },
   { name: 'MAIN COURSE', count: 6 },
 ]
+
+export const sample_users: any[] = [
+  {
+    name: "John Doe",
+    email: "john@gmail.com",
+    password: "12345",
+    address: "Toronto On",
+    isAdmin: true,
+  },
+  {
+    name: "Jane Doe",
+    email: "Jane@gmail.com",
+    password: "12345",
+    address: "Shanghai",
+    isAdmin: false,
+  },
+];

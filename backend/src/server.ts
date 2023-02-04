2bfeb3428f202324c3ef201403564c8491b51021
Dev-musab <Dev.tatatech@gmail.com>
2023-02-04 20:25:02 +0400

    re-create enviroment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+import path from 'path';
 dotenv.config();
 import express from "express";
 import cors from "cors";
@@ -13,14 +14,19 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     credentials:true,
-    origin:["http://localhost:4200"]
+    origin:["http://localhost:4200", "https://foodmenu.tatatech.co"]
 }));
 
 app.use("/api/foods", foodRouter);
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
 
-const port = 5000;
+app.use(express.static('public'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname,'public', 'index.html'))
+})
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log("Website served on http://localhost:" + port);
 })

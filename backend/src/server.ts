3f9bed88242d500f5e41408afbaffb9f1e0e17d2
Dev-musab <Dev.tatatech@gmail.com>
2023-02-04 20:52:17 +0400

    fix

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,6 @@
 import dotenv from 'dotenv';
 import path from 'path';
 dotenv.config();
-import path from 'path';
 import express from "express";
 import cors from "cors";
 import foodRouter from './routers/food.router';
